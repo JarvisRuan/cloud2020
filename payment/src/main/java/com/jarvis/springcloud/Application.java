13038a2e908a96f5130c49683621fba54886296a
ruanjj <1097077260@qq.com>
2020-04-29 23:54:09 +0800

    初始化payment项目

---
+++ payment/src/main/java/com/jarvis/springcloud/Application.java
@@ -0,0 +1,12 @@
+package com.jarvis.springcloud;
+
+/**
+ * Author : jarvis
+ * Date: 2020/4/29 23:48
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        springApplication.run(Application.class,args);
+    }
+}
